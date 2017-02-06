7038559e57e0cab3905a85af73e9d607e3b21bae
markoneil <mark.a.oneil@oscelot.org>
2017-02-05 22:55:42 -0500

    added config file and updated index js
    config file for local runtime, updated index.js to use.

---
+++ config/config.js
@@ -0,0 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+
+//use these when not set by cloud setup or using localhost for dev...
+var config = { 
+  lti_key: '54321',  
+  lti_secret: 'terces',
+  oauth_key: 'your-oauth-key',
+  oauth_secret: 'your-oauth-secret',
+  port: 3000,
+  host: 'localhost',
+  db: 'mongodb://localhost/signuplist'
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+var config = require('../config/config');
+
 var express = require('express');
 var lti = require('ims-lti');
 var _ = require('lodash');
@@ -5,8 +7,10 @@ var path = require('path');
 
 var router = express.Router();
 
-var lti_key = process.env.LTI_KEY;
-var lti_secret = process.env.LTI_SECRET;
+var lti_key = process.env.LTI_KEY || config.lti_key;
+var lti_secret = process.env.LTI_SECRET || config.lti_secret;
+var oauth_key = process.env.OAUTH_KEY || config.oauth_key;
+var oauth_secret = process.env.OAUTH_SECRET || config.oauth_secret;
 
 
 var course_uuid = "";
@@ -17,21 +21,34 @@ var return_url = "";
 
 var valid_session = false;
 
-if (lti_key == undefined) {
-  lti_key = "54321";
-  console.log('Using key from index.js: ');
+/* key and secret sanity checks - logged on startup */
+if (lti_key == config.lti_key) {
+  console.log('Using lti_key from index.js: ');
 } else {
-  console.log('Using key from process.env: ')
+  console.log('Using lti_key from process.env: ')
 }
 console.log(lti_key);
 
-if (lti_secret == undefined) {
-  lti_secret = "terces";
-  console.log('Using secret from index.js: ');
+if (lti_secret == config.lti_secret) {
+  console.log('Using lti_secret from index.js: ');
 } else {
-  console.log('Using secret from process.env: ');
+  console.log('Using lti_secret from process.env: ');
 }
 console.log(lti_secret);
+
+if (oauth_key == config.oauth_key) {
+  console.log('Using oauth_key from index.js: ');
+} else {
+  console.log('Using oauth_key from process.env: ')
+}
+console.log(oauth_key);
+
+if (oauth_secret == config.oauth_secret) {
+  console.log('Using oauth_secret from index.js: ');
+} else {
+  console.log('Using oauth_secret from process.env: ');
+}
+console.log(oauth_secret);
 
 /* Return home page from LTI Launch. */
 router.post('/lti', function(req, res, next) {
