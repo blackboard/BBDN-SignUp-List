16a35f55a226b8a1d2e82a39307776465e516129
markoneil <mark.a.oneil@oscelot.org>
2017-02-11 13:00:49 -0500

    course and system routes and schemas

--- routes/courses.js
+++ routes/courses.js
@@ -26,20 +26,57 @@ router.post('/', function(req, res, next) {
     });
 });
 
+
+
+/*
+ * GET /courses route to retrieve all the courses.
+ */
 router.get('/', function(req, res, next) {
-    //res.send('get all courses');
+    var query = Course.find({});
+    query.exec((err, courses) => {
+        if(err) res.send(err);
+        //If no errors, send them back to the client
+        res.json(courses);
+    });
+});
+
+/*
+ * GET /courses/:uuid route to retrieve a single course.
+ */
+router.get('/:id', function(req, res, next) {
+    //Query the DB and if no errors, return all the systems
+    Course.findOne({uuid: req.params.id}, (err, course) => {
+        if(err) res.send(err);
+        //If no errors, send them back to the client
+        res.json(course);
+    });
 });
 
 router.get('/:id', function(req, res, next) {
     res.send('get courses requested');
 });
 
+/*
+ * PUT /systems/:system_id route to update a single system.
+ */
 router.put('/:id', function(req, res, next) {
-    res.send('update course requested');
+    Course.findOne({uuid: req.params.id}, (err, course) => {
+        if(err) res.send(err);
+        Object.assign(course, req.body).save((err, course) => {
+            if(err) res.send(err);
+            res.json(course);
+        }); 
+    });
 });
 
+
+/*
+ * DELETE /courses/:id route to delete a single course.
+ */
 router.delete('/:id', function(req, res, next) {
-    res.send('delete course requested');
+    Course.remove({uuid : req.params.id}, (err, result) => {
+        res.status(204).send();
+    });
 });
 
 module.exports = router;
