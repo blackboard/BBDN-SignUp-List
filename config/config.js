6df55019c240c79c7914451810a5c97ff48c1a4f
Scott Hurrey <scott.hurrey@blackboard.com>
2017-02-13 10:33:03 -0500

    Update config.js
    resolve conflicts

--- config/config.js
+++ config/config.js
@@ -2,15 +2,16 @@ var env = process.env.NODE_ENV || 'development';
 
 //use these when not set by cloud setup or using localhost for dev...
 var config = {
-  lti_key: '12345',
-  lti_secret: 'secret',
-  oauth_key: 'd03caa33-1095-47b9-bc67-f5cd634430b1',
-  oauth_secret: 'QSFClAMu5KmoG8yFbHTi7pjhsseJl4uz',
+  lti_key: '54321',  
+  lti_secret: 'terces',
+  oauth_key: 'your-oauth-key',
+  oauth_secret: 'your-oauth-secret',
   port: 3000,
   host: 'localhost',
   rest_host: 'localhost',
   rest_port: 9877,
-  db: 'mongodb://localhost/signuplist'
+  db: 'mongodb://localhost/signuplist',
+  test_db: 'mongodb://localhost:27017/test_signuplistdb'
 };
 
 module.exports = config;
