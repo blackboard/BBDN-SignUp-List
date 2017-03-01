328569e89b1ee34a26fca5b22c2dc275a79d43cd
Mark O'Neil <mark.oneil@blackboard.com>
2017-03-01 15:04:55 -0500

    incremental

--- config/config.js
+++ config/config.js
@@ -2,17 +2,20 @@ var env = process.env.NODE_ENV || 'development';
 
 try {
     var keys = require('./keys.js');
+    if (keys.lti_key == undefined) {
+        keys = require('./keys.template.js');
+    }
 } catch (ex) {
     var keys = require('./keys.template.js');
 }
 
 //use these when not set by cloud setup or using localhost for dev...
 var config = {
-  lti_key: process.env.lti_key || keys.lti_key,
-  lti_secret: process.env.lti_secret || keys.lti_secret,
-  oauth_key: process.env.app_oauth_key || keys.oauth_key,
-  oauth_secret: process.env.app_oauth_secret || keys.oauth_secret,
-  port: process.env.app_port || 3000,
+  lti_key: keys.lti_key,
+  lti_secret: keys.lti_secret,
+  oauth_key: keys.oauth_key,
+  oauth_secret: keys.oauth_secret,
+  port: 3000,
   host: process.env.app_host || 'localhost',
   rest_host: process.env.app_target_url || 'localhost', //LMS Host Name
   rest_port: process.env.app_target_port || '3000', //LMS Port (may be left blank)
